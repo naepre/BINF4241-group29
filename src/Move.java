329c9eda15ae5102c02b71bee62923cf24e0c2e3
Wahlbar <fraese44@gmail.com>
2019-10-08 01:02:15 +0200

    move_function_updated

--- src/Move.java
+++ src/Move.java
@@ -4,20 +4,16 @@ import java.util.List;
 
 public class Move {
 
-    public int move(int position, int dicenumber, String playername, HashMap board) {
+    public int move(int position, int dicenumber, HashMap board) {
         Game game = new Game();
         int index = position + dicenumber;
         int new_position = 1;
         List square = new ArrayList();
         square = (List) board.get(index);
-        List start_square = new ArrayList();
-        start_square = (List) board.get(1);
         int square_type = (int) square.get(0);
         String occupation = (String) square.get(1);
-//        System.out.println(square_type);
-  //      System.out.println(occupation);
         if (square_type != 0) {
-            move(index, square_type, playername, board);
+            move(index, square_type, board);
         }
         else if (square_type == 0 && occupation == "null") {
             new_position = index;
