50813741c2c32b888fd61e0616b1417eb3333c6d
Wahlbar <simon@22nov.zyxel.com>
2019-10-21 22:10:03 +0200

    pawn eat

--- Chess/src/Pawn.java
+++ Chess/src/Pawn.java
@@ -26,33 +26,66 @@ public class Pawn extends Figures {
         return this.color;
     }
 
-    public ArrayList move(int x, int y, char[][][] board) {
+    public ArrayList move(int x, int y, char playercolor, int movetype, char[][][] board) {
         ArrayList possibleMoves = new ArrayList<>();
 
-        if (board[y - 1][x].length != 0) {
+        if (movetype == 0) {
 
-            int[] figPos = {y-1, x};
-            Object[] cell = {board[y-1][x], figPos};
-            possibleMoves.add(cell);
+            if (playercolor == 'b') {
+                if (board[y - 1][x].length != 0) {
 
-        }
-        if (board[y + 1][x].length != 0) {
+                    int[] figPos = {y - 1, x};
+                    Object[] cell = {board[y - 1][x], figPos};
+                    possibleMoves.add(cell);
 
-            int[] figPos = {y+1, x};
-            Object[] cell = {board[y+1][x], figPos};
-            possibleMoves.add(cell);
+                }
+                if (y == 3 && board[y - 1][x].length == 0 && board[y - 2][x].length != 0) { //a black pawn start two space move
+                    int[] figPos = {y - 2, x};
+                    Object[] cell = {board[y - 2][x], figPos};
+                    possibleMoves.add(cell);
+                }
+            }
+            if (playercolor == 'w') {
+                if (board[y + 1][x].length != 0) {
+
+                    int[] figPos = {y + 1, x};
+                    Object[] cell = {board[y + 1][x], figPos};
+                    possibleMoves.add(cell);
+                }
+
+                if (y == 4 && board[y + 1][x].length == 0 && board[y + 2][x].length != 0) { //a white pawn start two space move
+                    int[] figPos = {y + 2, x};
+                    Object[] cell = {board[y + 2][x], figPos};
+                    possibleMoves.add(cell);
+                }
+            }
+        } else {
+            if (playercolor == 'b') {
+                if (board[y - 1][x - 1].length != 0) {
+                    int[] figPos = {y - 1, x - 1};
+                    Object[] cell = {board[y - 1][x - 1], figPos};
+                    possibleMoves.add(cell);
+                }
+                if (board[y - 1][x + 1].length != 0) {
+                    int[] figPos = {y - 1, x + 1};
+                    Object[] cell = {board[y - 1][x + 1], figPos};
+                    possibleMoves.add(cell);
+                }
+            } else if (playercolor == 'w') {
+                if (board[y + 1][x - 1].length != 0) {
+                    int[] figPos = {y + 1, x - 1};
+                    Object[] cell = {board[y + 1][x - 1], figPos};
+                    possibleMoves.add(cell);
+                }
+                if (board[y + 1][x + 1].length != 0) {
+                    int[] figPos = {y + 1, x + 1};
+                    Object[] cell = {board[y + 1][x + 1], figPos};
+                    possibleMoves.add(cell);
+                }
+            }
+
+            return possibleMoves;
         }
-        if (y == 3 && board[y - 1][x].length == 0 && board[y - 2][x].length != 0) { //a black pawn start two space move
-            int[] figPos = {y-2, x};
-            Object[] cell = {board[y-2][x], figPos};
-            possibleMoves.add(cell);
-        }
-        if (y == 4 && board[y + 1][x].length == 0 && board[y + 2][x].length != 0) { //a white pawn start two space move
-            int[] figPos = {y+2, x};
-            Object[] cell = {board[y+2][x], figPos};
-            possibleMoves.add(cell);
-        }
-        return possibleMoves;
     }
 }
 
