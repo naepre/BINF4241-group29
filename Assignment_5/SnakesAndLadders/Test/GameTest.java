1d652a1dadd53024fed6be2d7a17d20c0ad969e7
Clive <clivecharles.javara@uzh.ch>
2019-12-08 09:53:56 +0100

    Unit tests for game and turn class

--- Assignment_5/SnakesAndLadders/Test/GameTest.java
+++ Assignment_5/SnakesAndLadders/Test/GameTest.java
@@ -7,6 +7,13 @@ import static org.junit.Assert.*;
 public class GameTest {
     Game game = new Game();
     Square square = new Square();
+    Object[][] test = new Object[][]
+            {
+                    {0, 0, null},{1, 2, null},{2, 0, null},{3, 0, null},
+                    {4, 4, null},{5, 0, null},{6, -3, null},{7, 0, null},
+                    {8, 0, null},{9, -4, null},{10, 0, null},{11, 0, null}, {12, 0, null}
+            };
+
 
     @Before
     public void setup() {
@@ -24,15 +31,31 @@ public class GameTest {
 
     @Test
     public void TestUpdatePlayersCurrentPosition() {
-        game.updatePlayersCurrentPosition("clive",0);
-        Assert.assertEquals(0,game.playersCurrentPosition.get("clive"));
+
+        //Arrange
+        game.playersCurrentPosition.put("clive",2);
+        game.playersCurrentPosition.put("Nael",6);
+        //Act && Assert for player 1 (Clive)
+        game.updatePlayersCurrentPosition("clive",5);
+        Assert.assertEquals(5,game.playersCurrentPosition.get("clive"));
+
+        //Act && Arrange for player 2 (Nael)
+        game.updatePlayersCurrentPosition("Nael",3);
+        Assert.assertEquals(3,game.playersCurrentPosition.get("Nael"));
     }
 
     @Test
     public void TestGetPlayersCurrentPosition() {
-        game.playersCurrentPosition.put("clive", 0);
-        int pos = game.getPlayersCurrentPosition("clive");
-        Assert.assertEquals(0,pos);
+        //Arrange
+        game.playersCurrentPosition.put("clive",0);
+        game.playersCurrentPosition.put("Nael",11);
+        //Act && Assert Player 1 (clive)
+        int player1pos = game.getPlayersCurrentPosition("clive");
+        Assert.assertEquals(0,player1pos);
+
+        //Act && Assert Player 2 (Nael)
+        int player2pos = game.getPlayersCurrentPosition("Nael");
+        Assert.assertEquals(11,player2pos);
     }
 
     @Test
@@ -53,12 +76,24 @@ public class GameTest {
 
     @Test
     public void TestCheckWinCondition() {
-        //Square square = new Square();
-        //Object[][] board = square.createSquare();
-        // game.board[12][2] = "clive";
-        //boolean bool = game.checkWinCondition();
-        //System.out.println("Initial board: " +bool);
-        // Assert.assertEquals(true,bool);
-
+        /**
+         * When the player moves to last square function returns true
+         */
+        //Arrange
+        game.board = square.createSquare();
+        game.board[12][2]= "clive";
+        //Act
+        boolean checkwin= game.checkWinCondition();
+        //Assert
+        Assert.assertEquals(true,checkwin);
+        /**
+         * When the last square is empty/null
+         */
+        //Arrange
+        game.board[12][2]= null;
+        //Act
+        boolean checknull= game.checkWinCondition();
+        //Assert
+        Assert.assertEquals(false,checknull);
     }
 }
--- Assignment_5/SnakesAndLadders/Test/TurnTest.java
+++ Assignment_5/SnakesAndLadders/Test/TurnTest.java
@@ -1,30 +1,99 @@
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Assert;
 import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class TurnTest {
+    ArrayList<String> players = new ArrayList();
+
     Square square = new Square();
+    Object[][] test = new Object[][]
+            {
+                    {0, 0, null},{1, 2, null},{2, 0, null},{3, 0, null},
+                    {4, 4, null},{5, 0, null},{6, -3, null},{7, 0, null},
+                    {8, 0, null},{9, -4, null},{10, 0, null},{11, 0, null}, {12, 0, null}
+            };
 
     @Before
     public void setup() {
-
-
+        players.add("clive");
+        players.add("tim");
 
     }
 
 
     @org.junit.Test
     public void TestMove() {
-        int position = 2 ;
-        int dicenumber = 6;
+        /**
+         * When the player moves to a square which is not a snake or a ladder
+         */
+        int[] startpos = {0, 2, 5, 7,10};
+        int [] dicenumber = {1,2,3,4,5,6};
         Turn turn = new Turn();
         Object[][] board = square.createSquare();
-        int newposition = turn.move(position,dicenumber,board);
-        Assert.assertEquals(2,newposition);
+        /**
+         * When the player moves to a square which is not a snake or a ladder
+         */
+        int normalcase = turn.move(startpos[0],dicenumber[4],board);
+        Assert.assertEquals(5,normalcase);
+        /**
+         * When the player moves to a square which is a snake
+         */
+        int snakecase = turn.move(startpos[2],dicenumber[3],board);
+        Assert.assertEquals(5,snakecase);
+        /**
+         * When the player moves to a square which is a ladder
+         */
+        int laddercase = turn.move(startpos[1],dicenumber[1],board);
+        Assert.assertEquals(8,laddercase);
+        /**
+         * Test when a player rolls a dice number larger then the number of squares away the finish field
+         */
+        int stepcase = turn.move(startpos[4],dicenumber[5],board);
+        Assert.assertEquals(8,stepcase);
+        /**
+         * Test when a player exactly lands on the final square
+         */
+        int lastsquare = turn.move(startpos[3],dicenumber[4],board);
+        Assert.assertEquals(12,lastsquare);
     }
+
+    /**
+     * Normal case simply put the player in its appropriate place
+     */
 
     @org.junit.Test
     public void updatePlayerPos() {
+        Turn update = new Turn();
+        Object[][] board1 = square.createSquare();
+        ArrayList<String> copyPlayer = (ArrayList<String>) players.clone();
+        board1[0][2] = copyPlayer;
+        System.out.println("fghfghf"+ Arrays.deepToString(board1));
+
+        Object[][] returnedboard = update.updatePlayerPos(0,3,"clive",board1);
+        Object output = returnedboard[3][2] ;
+        Object expected = test[3][2] = "clive" ;
+        Object prevpos =  returnedboard[0][2];
+
+        /**
+         * Normal case simply update player position according to current-position, new-position and player name
+         */
+        Assert.assertEquals(expected,output);
+        /**
+         * Test to see if the a player was removed from its starting postion
+         */
+        Object playerinstartpos = returnedboard[0][2];
+
+        System.out.println("fghfghf"+ prevpos);
+
+        // Assert.assertEquals(playerinstartpos.);
+
+
+
+
     }
 }
